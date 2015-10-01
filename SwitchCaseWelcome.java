29579bba6cb42d4ad7d8a26a98098e54d4ae5fdc
unknown <twg2997@AN112503-CT103.nvstu.edu>
2015-10-01 17:12:20 -0400

    1234

---
+++ SwitchCaseWelcome.java
@@ -0,0 +1,40 @@
+import javax.swing.JOptionPane;
+public class SwitchCaseWelcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String userRole = "";
+		userRole = JOptionPane.showInputDialog("Please select your role.");
+			switch(userRole) {
+				case "Administrator":
+					JOptionPane.showMessageDialog(null, "Welcome " + userRole + ".");
+					break;
+				
+				case "Faculty":
+					JOptionPane.showMessageDialog(null, "Welcome " + userRole + ".");
+					break;
+				
+				case "Staff":
+					JOptionPane.showMessageDialog(null, "Welcome " + userRole + ".");
+					break;
+				
+				case "Student":
+					JOptionPane.showMessageDialog(null, "Welcome " + userRole + ".");
+					break;
+				
+				case "Guest":
+					JOptionPane.showMessageDialog(null, "Welcome " + userRole + ".");
+					break;
+					
+			
+		}
+		
+		
+		
+		
+		
+		}
+		
+
+	}
+
