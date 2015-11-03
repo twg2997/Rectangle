12f13f65a44a3b81ff5ea7e187c34ea1772e03c4
unknown <twg2997@AN112503-CT103.nvstu.edu>
2015-11-03 17:43:22 -0500

    dsdsd

--- Car.java
+++ Car.java
@@ -1,29 +1,79 @@
-import java.util.Scanner;
-public class Delimiter {
+public class Car {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;//Each variable represents a numeric input value
+	private String color;
+	private int horsePower;
+	private String engineSize;
+	
+	public Car() {
+		this.color = "color";
+		this.horsePower = 0;
+		this.engineSize = "engineSize";
 		
-		Scanner keyboard1 = new Scanner(System.in);
-		keyboard1.useDelimiter(",");//Separates each number using a comma
-		System.out.println("Enter ten numbers separated by a comma to find the average.");
-		n1 = keyboard1.nextInt();//Stores the number input on each line to a variable
-		n2 = keyboard1.nextInt();
-		n3 = keyboard1.nextInt();
-		n4 = keyboard1.nextInt();
-		n5 = keyboard1.nextInt();
-		n6 = keyboard1.nextInt();
-		n7 = keyboard1.nextInt();
-		n8 = keyboard1.nextInt();
-		n9 = keyboard1.nextInt();
-		n10 = keyboard1.nextInt();
-		
-		int sum = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10;//Calculates sum
-		System.out.println("\nSum: " + sum);
-		int average = sum/10;//Calculates average
-		System.out.println("\nThe average is " + average + ".");
+	}
+	
+	public Car(String color, int horsePower, String engineSize) {
+		this.color = color;
+		this.horsePower = horsePower;
+		this.engineSize = engineSize;
+	}
+	
+	public String getcolor() {
+		return this.color;
 	}
 
+	public void setcolor(String color) {
+		this.color = color;
+	}
+
+	public int gethorsePower() {
+		return this.horsePower;
+	}
+
+	public void sethorsePower(int horsePower) {
+		this.horsePower = horsePower;
+	}
+
+	public String getengineSize() {
+		return this.engineSize;
+		
+	}
+	
+	public void setengineSize(String engineSize) {
+		this.engineSize = engineSize;
+	}
+	
+	public String toString() {
+		return "Car color is " + this.color + ", the horse-power is " + this.horsePower + ", and the engine size is " + engineSize + ".";
+	}
+
+
 }
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
 
